43448458b3c480197bd980180fa2424353702e8f
Romeu Gomes <Plasticoo@users.noreply.github.com>
2013-11-18 18:12:37 +0000

    Fixed output
    In the end of all output, there was a line that said "None". Now it's fixed.

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ def main():
     if len(sys.argv) == 2:
         word = sys.argv[1]
         hashing = CryptoLib(word)
-        print hashing.CalculateAllComplete()
+        hashing.CalculateAllComplete()
     elif len(sys.argv) == 3:
         word = sys.argv[1]
         salt = sys.argv[2]
         hashing = CryptoLib(word)
-        print hashing.CalculateAllCompleteSalt(salt)
+        hashing.CalculateAllCompleteSalt(salt)
     else:
         print "How to use: python main.py [word] [salt]"
 
